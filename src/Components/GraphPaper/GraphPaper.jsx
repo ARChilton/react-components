c9fcd3be31bf3e20cc131b8771dbedc46d7ca2cc
Adam Chilton <adam@dsan.co.uk>
2021-01-14 21:23:56 +0000

    add width and height css

--- src/Components/GraphPaper/GraphPaper.jsx
+++ src/Components/GraphPaper/GraphPaper.jsx
@@ -5,5 +5,7 @@ const GraphPaper = styled('div')`
     `linear-gradient(${gridColor}, 2px, transparent 2px), linear-gradient(90deg, ${gridColor},  2px, transparent 2px), linear-gradient(rgba(150, 150, 150, .3) 1px, transparent 1px), linear-gradient(90deg, rgba(150, 150, 150, .3) 1px, transparent 1px)`};
   background-size: 100px 100px, 100px 100px, 20px 20px, 20px 20px;
   background-position: -2px -2px, -2px -2px, -1px -1px, -1px -1px;
+  width: 100%;
+  height: 100%;
 `
 export default GraphPaper
