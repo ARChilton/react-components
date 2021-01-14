970c62bbc407bef8dbb9923b607dfca137b48f61
Adam Chilton <adam@dsan.co.uk>
2021-01-14 21:33:16 +0000

    story for IPhoneX

---
+++ src/Modules/IPhoneX/IPhoneX.stories.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import Iphonex from './IPhoneX'
+
+export default {
+  title: 'Modules/IPhoneX',
+  component: Iphonex,
+}
+
+export const Default = ({ ...args }) => <Iphonex {...args} />
